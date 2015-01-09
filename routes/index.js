f11f2b9062059dc4312fdc8de81ea178f0b24552
whuang8 <huang554@purdue.edu>
2015-01-08 21:47:12 -0500

    nothing done

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Purdue Web and Design Club' });
 });
 
 module.exports = router;
